37b26a3ffe8fd4e827307f2f94098155349eec9c
henry dell <prez.gumiel@gmail.com>
2022-08-16 00:13:00 -0400

    uno1

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://prueba-inicio-mean.herokuapp.com/api'
-  baseUrl: 'http://ec2-18-208-137-236.compute-1.amazonaws.com:3000/api'
+  baseUrl: 'http://18.208.137.236:3000/api'
 };
