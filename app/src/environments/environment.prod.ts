40fdf7fe733af85055be2adc0ee6bd700be43a18
Juanks <alavjc@gmail.com>
2022-08-19 22:42:38 -0400

    eliminacion de dist

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // baseUrl: 'https://prueba-inicio-mean.herokuapp.com/api'
-  baseUrl: 'http://18.208.137.236:3000/api'
+  baseUrl: 'http://18.208.137.236:2000/api'
 };
